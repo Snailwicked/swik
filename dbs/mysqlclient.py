f389f51287ba3c2cdabed678dce2e7f7e990e0d4
bsnail <2575652913@qq.com>
2019-03-28 11:28:21 +0800

    mysql 添加注释 添加通过xpath,url,header获取相关内容的爬虫工具类

--- dbs/mysqlclient.py
+++ dbs/mysqlclient.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 import pymysql
 
-'''mysql 客户端封装接口'''
-class mysqlClient():
-    def __init__(self):
-        self.mysql_conf = {'host': '182.254.198.218', 'port': 3306, 'user': 'root', 'password': 'vrv123123',
+'''
+mysql 客户端封装接口
+用法：
+    conf = {'host': '182.254.198.218', 'port': 3306, 'user': 'root', 'password': 'vrv123123',
                            'db_name': 'crawler',
                            'db_table': 'information'}
-
+    mysqlclient = mysqlClient(conf)
+    sql =  'insert into information (url,author,title,postTime) values (%s,%s,%s,%s)'
+    data = ('http://www.sohu.com/a/304312045_123753', '中国新闻网', '美军证实印度导弹命中卫星 莫迪：具历史性意义', '2019-03-28 09:32:14')
+    mysqlclient.insert(sql,data)
+    mysqlclient.close()
+'''
+class mysqlClient():
+    def __init__(self,conf = None):
+        self.mysql_conf = conf
         self.conn = pymysql.connect(host=self.mysql_conf['host'], port=self.mysql_conf['port'],
                                     user=self.mysql_conf['user'], passwd=self.mysql_conf['password'],
                                     db=self.mysql_conf['db_name'], charset="utf8")
@@ -40,7 +48,7 @@ class mysqlClient():
             self.conn.rollback()
 
 
-    def close_all(self):
+    def close(self):
          try:
              if self.cursor is not None:
                  self.cursor.close()
---
+++ utils/spiderutils/xpathtexts.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import requests,re
+from lxml import etree
+'''
+传入url,header通过xPath路径获取html中的数据
+用法：
+    url = "http://www.sohu.com/a/304311876_123753"
+    X_path= "//article[@class = 'article']//p//text()"
+    header = {
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',}
+    xpt = xPathTexts()
+    contens = xpt.get_contents(url,X_path,header)
+'''
+class xPathTexts(object):
+    def __init__(self, url = None,X_path=None ,header = None):
+        self.url = url
+        self.X_path = X_path
+        self.contents = []
+        self.headers = header
+
+    def getHtml(self):
+        '''
+        获取self.url 的 html
+        :return: html
+        '''
+        resp = requests.get(self.url,headers= self.headers)
+        charset = None
+        try:
+            reg = '<meta .*(http-equiv="?Content-Type"?.*)?charset="?([a-zA-Z0-9_-]+)"?'
+            bianma = re.findall(reg, resp.text)[0][1]
+        except:
+            bianma = ""
+        if bianma!="":
+            charset = bianma.lower()
+        resp.encoding = charset
+        return resp.text
+
+    def get_contents(self,url,X_path,header):
+        '''
+        :param url: 'http://www.sohu.com/a/304311876_123753'
+        :param X_path: '//article[@class = 'article']//p//text()'
+        :param header: {
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9',}
+        :return: '原标题：日媒：日本新年号拟于4月1日中午前后公布中新网3月28日电 据日本共同社报道，当地时间27日，多名相关人士透露，日本政府已为4月1日中午'
+        '''
+        self.url = url
+        self.X_path = X_path
+        self.contents = []
+        self.headers = header
+        self.html = self.getHtml()
+        contens = []
+        for item in etree.HTML(str(self.html)).xpath(self.X_path):
+            contens.append(str(item).strip())
+        return contens
