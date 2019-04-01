1cf44712465f62c6b39fae4291c80aac15af0eb6
bsnail <2575652913@qq.com>
2019-04-01 15:36:56 +0800

    异步编程

--- utils/baseutils/async.py
+++ utils/baseutils/async.py
@@ -1,63 +1,47 @@
-import time
 import asyncio
-import threading
-import requests
-url = "http://91xinshang.com/xianzhi/NGQ2Z05JZWtwVVU9.html"
-def run3():
-    for i in range(5):
-        html = requests.get(url).text
-        print("同步代码",time.time())
+from aiohttp import ClientSession
+import re
 
-class myThread(threading.Thread):
+class ASymain():
+
     def __init__(self):
-        threading.Thread.__init__(self)
+        self.loop = asyncio.get_event_loop()
+        self.tasks = []
+        self.charset = "utf-8"
 
-    def run(self):  # 把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
-        self.function()
-    def function(self):
-        html = requests.get(url).text
-        print("线程代码",time.time())
+    async def getHTML(self,url):
+        async with ClientSession() as session:
+            async with session.get(url) as response:
+                try:
+                    reg = '<meta .*(http-equiv="?Content-Type"?.*)?charset="?([a-zA-Z0-9_-]+)"?'
+                    bianma = re.findall(reg, await response.text())[0][1]
+                except:
+                    bianma = ""
+                if bianma != "":
+                    self.charset = bianma.lower()
+                print(self.charset)
+                response.encoding = self.charset
+                try:
+                    return await response.text()
+                except:
+                    print("pass")
+    def run(self,urls):
+        for url in urls:
+            task = asyncio.ensure_future(self.getHTML(url))
+            self.tasks.append(task)
+        yield self.loop.run_until_complete(asyncio.gather(*self.tasks))
 
-
-def run2():
-    thread1 = myThread()
-    thread2 = myThread()
-    thread3 = myThread()
-    thread4 = myThread()
-    thread5 = myThread()
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-    thread5.start()
-
-# async def hello():
-#     html = requests.get(url).text
-#     print('异步代码:%s' % time.time())
-#
-# def run():
-#     for i in range(5):
-#         loop.run_until_complete(hello())
-# loop = asyncio.get_event_loop()
-
-
-class myThread2(threading.Thread):
-    def __init__(self,name):
-        threading.Thread.__init__(self)
-        self.function = name
-
-    def run(self):  # 把要执行的代码写到run函数里面 线程在创建后会直接运行run函数
-        try:
-            self.function()
-        except:
-            pass
-
-if __name__ =='__main__':
-    my2 = myThread2(run2())
-    # my3 = myThread2(run())
-    my1 = myThread2(run3())
-    my1.start()
-    my2.start()
-    # my3.start()
+    def start(self,urls):
+        self.loop = asyncio.get_event_loop()
+        for item in self.run(urls):
+            print(item)
 
 
+
+
+
+if __name__ == '__main__':
+    urls = ["http://news.sohu.com/","",""]
+
+    am =ASymain()
+    am.start(urls)
