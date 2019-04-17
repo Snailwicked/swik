05acf60cb9b341c1b99dd643e021e8f4597a91cf
wangkai <1533260667@qq.com>
2019-04-17 10:17:09 +0800

    登录功能

--- webapplication/app.py
+++ webapplication/app.py
@@ -1,14 +1,14 @@
-from __future__ import unicode_literals
 from flask import (Flask, render_template, redirect, url_for, request, flash)
 from flask_bootstrap import Bootstrap
 from flask_login import login_required, login_user, logout_user, current_user
-from .forms import TodoListForm, LoginForm
-from .ext import db, login_manager
-from .models import WebInfo, User
+from webapplication.forms import TodoListForm, LoginForm
+from webapplication.ext import db, login_manager
+from webapplication.models import WebInfo, User
 
 
 SECRET_KEY = 'This is my key'
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
 app.secret_key = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:BlueSnail123!@101.132.113.50/spider_manage"
@@ -24,7 +24,7 @@ def show_todo_list():
     form = TodoListForm()
     if request.method == 'GET':
         webinfos = WebInfo.query.all()
-        return render_template('index.html', webinfos=webinfos, form=form)
+        return render_template('index', webinfos=webinfos, form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
