068223ffeac4436f33102aa1d45b4c715209fa7e
wangkai <1533260667@qq.com>
2019-04-17 14:07:38 +0800

    登录功能

--- webapplication/models.py
+++ webapplication/models.py
@@ -1,6 +1,6 @@
 from .ext import db
 from flask_login import UserMixin
-import time
+import datetime
 
 
 class WebInfo(db.Model):
@@ -9,14 +9,14 @@ class WebInfo(db.Model):
     url = db.Column(db.String(1024), nullable=False)
     web_name = db.Column(db.String(1024), nullable=True)
     info = db.Column(db.String(1024), nullable=True)  # 备用的
-    add_time = db.Column(db.DateTime, nullable=True)
+    add_time = db.Column(db.String(100), nullable=True)
     status = db.Column(db.Integer, nullable=True, default=0)  # 0说明没有采集  1被采集了
     agent = db.Column(db.Integer, nullable=True)  # 0国内  1国外
     sort = db.Column(db.Integer, nullable=True)  # 1 通用型 2 登录型 3 特殊型
 
     def __init__(self, url):
         self.url = url
-        self.add_time = time.time()
+        self.add_time = datetime.datetime.now()
 
 
 class User(UserMixin, db.Model):
