0d8377f277e6f185db00ce7d0b3de8df4f18924e
wangkai <1533260667@qq.com>
2019-04-22 10:02:14 +0800

    删除操作

--- spiders/sohu.py
+++ spiders/sohu.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 import time,hashlib
 import requests,re
 from lxml import etree
--- webapplication/app.py
+++ webapplication/app.py
@@ -5,11 +5,30 @@ from flask_login import login_required, login_user, logout_user, current_user
 from webapplication.forms import TodoListForm, LoginForm
 from webapplication.ext import db, login_manager
 from webapplication.models import WebInfo, User
+from flask_nav import Nav
+from flask_nav.elements import *
 
 
 SECRET_KEY = 'This is my key'
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+nav = Nav()
+nav.register_element('top', Navbar(u'爬虫管理后台',
+                                    View(u'主页', 'show_todo_list'),
+                                    View(u'网址管理', 'show_todo_list'),
+                                    Subgroup(u'爬虫管理',
+                                             View(u'在启动爬虫列表', 'show_todo_list'),
+                                             Separator(),
+                                             View(u'待启动爬虫列表', 'show_todo_list'),
+                                    ),
+                                    Subgroup(u'数据统计',
+                                            View(u'服务器监控', 'show_todo_list'),
+                                            Separator(),
+                                            View(u'爬虫监控', 'show_todo_list'),
+                                            ),
+                                    View(u'权限管理', 'show_todo_list'),
+))
+nav.init_app(app)
 app.secret_key = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:BlueSnail123!@101.132.113.50/spider_manage"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -27,7 +46,7 @@ def show_todo_list():
         pagination = WebInfo.query.order_by(WebInfo.add_time.desc()).paginate(page, per_page=10, error_out=False)
         webinfos = pagination.items
         # webinfos = WebInfo.query.all()
-        return render_template('index.html', webinfos=webinfos, form=form)
+        return render_template('index.html', webinfos=webinfos, form=form, pagination=pagination)
     if form.validate_on_submit():
         webinfo = WebInfo(form.url.data, form.web_name.data, form.status.data, form.agent.data, form.sort.data)
         db.session.add(webinfo)
